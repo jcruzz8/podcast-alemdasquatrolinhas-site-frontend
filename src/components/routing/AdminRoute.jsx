e1bd4c2a4e4365d40918ec6cd64c6088d59855f7
José Cruz <josejmcruz10@gmail.com>
2025-11-13 03:57:09 +0000

    Atualiza o fundo preto da paginda de edit post na dashboard de admin

--- src/components/routing/AdminRoute.jsx
+++ src/components/routing/AdminRoute.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { Navigate, Outlet } from 'react-router-dom';
+
+import '../../pages/AdminDashboard.css';
 
 // Este componente é o nosso "Guarda-Costas"
 function AdminRoute() {
@@ -10,6 +12,17 @@ function AdminRoute() {
     // 2. Verifica se o utilizador é um admin
     //    (O 'user?' é "optional chaining": só tenta ler 'role' se 'user' não for null)
     const isAdmin = isLoggedIn && user?.role === 'admin';
+
+    // Este 'useEffect' agora aplica-se a TODAS as rotas de admin
+    useEffect(() => {
+        const originalColor = document.body.style.backgroundColor;
+        document.body.style.backgroundColor = '#444444'; // O fundo cinza-escuro
+
+        // Esta função de limpeza só corre se sairmos da secção de Admin
+        return () => {
+            document.body.style.backgroundColor = originalColor || '#f4f4f4';
+        }
+    }, []); // Corre uma vez quando entramos no Admin
 
     if (isAdmin) {
         // 3. Se for admin: Deixa-o passar. O <Outlet /> é o "placeholder"
