d0334a9f5a5f306573d07d8237a8e9c31dbfcf10
José Cruz <josejmcruz10@gmail.com>
2025-11-13 01:55:51 +0000

    Torna a baseURL da API dinâmica com VITE_API_UR

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     // Define o URL base para todos os pedidos
     // Assim, em vez de axios.post('http://localhost:5000/api/auth/login', ...)
     // podemos simplesmente fazer api.post('/auth/login', ...)
-    baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
